fd406cb85e06bf6190a178fe027708b8a99714ee
shereenbashar2 <eng.shereenbashar@gmail.com>
2023-11-14 08:22:13 +0200

    add tpes

--- src/controllers/imageController.ts
+++ src/controllers/imageController.ts
@@ -79,7 +79,7 @@ const handleErrors = (res: Response, errors: Result<ValidationError>) => {
   return res.status(400).json({ errors: errors.array() });
 };
 // Controller to process and serve images
-export const processImage = async (req: Request, res: Response):  => {
+export const processImage = async (req: Request, res: Response) => {
   try {
     // Validate image parameters
     const validationRules = validateImageParameters();
